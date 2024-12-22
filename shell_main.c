0704cb4a51a7ed6b951342a31a5ce40876b982b4
root <root@DESKTOP-NDVQC7F.localdomain>
2024-12-22 15:34:57 +0100

    shell_main.c updated

--- shell_main.c
+++ shell_main.c
@@ -9,6 +9,38 @@ pid_t pid;
 int status;
 while (1)
 {
-
+if (isatty(STDIN_FILENO))
+printf("$ ");
+readline = read_line();
+if (readline == NULL)
+break;
+tokens[0] = strtok(readline, " \n");
+tokens[1] = NULL;
+if (token[0] == NULL)
+{
+free(readline);
+continue;
 }
+pid = fork();
+if (pid == -1)
+{
+perror ("Fork failed");
+free (readline);
+continue;
+}
+if (pid == 0)
+{
+if (execve(tokens[0], tokens, environ) == -1)
+{
+perror ("command not found");
+exit (127);
+}
+}
+else
+{
+wait (&status);
+}
+free (readline);
+}
+return (0);
 }
