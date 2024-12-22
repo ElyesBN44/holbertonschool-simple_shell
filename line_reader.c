646567b4fdd6a7c648dc9e090638d3f357efa977
root <root@DESKTOP-NDVQC7F.localdomain>
2024-12-22 17:18:49 +0100

    line_reader.c updated

--- line_reader.c
+++ line_reader.c
@@ -5,5 +5,19 @@
 char *read_line(void)
 {
 char *buffer = NULL;
-
+size_t buffer_size = 0;
+ssize_t line_length;
+line_length = getline(&buffer, &buffer_size, stdin);
+if (line_length == -1)
+{
+free(buffer);
+if (feof(stdin))
+exit(0);
+else
+{
+perror("Error reading input");
+exit(1);
+}
+}
+return buffer;
 }
