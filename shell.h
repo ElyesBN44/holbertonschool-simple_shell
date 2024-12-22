01ed244fd315a2645f327ba1f41004d098928e79
root <root@DESKTOP-NDVQC7F.localdomain>
2024-12-22 15:49:59 +0100

    shell.h updated

---
+++ shell.h
@@ -0,0 +1,15 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+int main(void);
+
+#endif
