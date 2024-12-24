7dc5964afd0e19cdfc149005728046558eeffb75
root <root@Elyes.>
2024-12-24 11:12:07 +0100

    functional parse command

--- parse_the_line.c
+++ parse_the_line.c
@@ -1,9 +1,22 @@
 #include "shell.h"
 /**
-*parse_line - Execute a command that analyzes & breaks down line.
-*Return: Exits the executed command.
+*parse_line - Parses the input line into an array.
+*@readline: The string that will be parsed.
+*@separate: To separate the array into words.
+*Return: The array of words.
 */
-int parse_the_line()
+char **parse_the_line(char *readline, const char *separate)
 {
-return (0);
+int i = 0, size = SIZE;
+char **tokens=(char **) malloc(size * sizeof(char * )), *token = NULL;
+token = strtok(readline, separate);
+while (token != NULL)
+{
+tokens[i] = strdup(token);
+i++;
+token = strtok(NULL, separate);
+}
+tokens[i] = NULL;
+free(readline);
+return(tokens);
 }
