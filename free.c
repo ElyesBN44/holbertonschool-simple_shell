ca0f216ffbc64be7e23c7726825716231d1c1ef7
root <root@Elyes.>
2024-12-24 11:18:54 +0100

    fixed betty issues in free

--- free.c
+++ free.c
@@ -4,12 +4,12 @@
  * free_d_p - Frees a dynamically allocated array of pointers.
  * @array: The double pointer to free.
  */
- void free_d_p(char **array)
- {
+void free_d_p(char **array)
+{
 int i;
 if (array == NULL)
 return;
-for (i = 0; array [i] != NULL; i++)
+for (i = 0; array[i] != NULL; i++)
 {
 free(array[i]);
 }
