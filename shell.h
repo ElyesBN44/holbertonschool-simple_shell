af296989af33199d7fc02df8ec02237251fa4a8a
root <root@Elyes.>
2024-12-26 12:09:22 +0100

    added the check_path function

--- shell.h
+++ shell.h
@@ -17,6 +17,7 @@ int main(void);
 char *line_read(void);
 char **parse_the_line(char *readline, const char *separate);
 int process_line(char **tokens, char *path);
+char *check_path(char *command);
 void _env(void);
 
 void free_d_p(char **array);
