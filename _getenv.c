afb7e52802404b360c289a22ae2fff4e665af1aa
root <root@Elyes.>
2024-12-27 13:05:36 +0100

    fixed betty

--- _getenv.c
+++ _getenv.c
@@ -14,8 +14,8 @@ for (i = 0; environ[i] != NULL; i++)
 {
 if (strncmp(environ[i], name, name_len) == 0 && environ[i][name_len] == '=')
 {
-return (environ[i] + name_len +1);
+return (environ[i] + name_len + 1);
 }
 }
 return (NULL);
-} 
+}
