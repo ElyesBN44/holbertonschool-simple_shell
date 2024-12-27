f0fadd3873b8db420bdab1f6b4830d6c0380d6dd
root <root@Elyes.>
2024-12-27 12:32:12 +0100

    changed the get env function

--- check_path.c
+++ check_path.c
@@ -8,7 +8,7 @@
 
 char *check_path(char *command)
 {
-char *array[1000], *path = strdup(getenv("PATH"));
+char *array[1000], *path = strdup(_getenv("PATH"));
 char *path_token = strtok(path, ":");
 int i = 0;
 char *final_path;
